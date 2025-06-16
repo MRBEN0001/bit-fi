aad21ee263232c7b6000b1bc96e5c4615b4066d1
unknown <anenebenjaminjnr@gmail.com>
2025-06-16 17:05:46 +0100

    added dd in withdrawal method

--- app/Http/Controllers/WithdrawalController.php
+++ app/Http/Controllers/WithdrawalController.php
@@ -55,8 +55,10 @@ class WithdrawalController extends Controller
 
                 // Flash success message
                 session()->flash('success', $withdraw['message']);
+                dd("Email sent succesfully");
                 return redirect('/withdrawals');
             } catch (\Throwable $error) {
+                dd($error->getMessage());
                 Log::error('SMTP network error: ' . $error->getMessage());
                 return redirect('/withdrawals');
             }
